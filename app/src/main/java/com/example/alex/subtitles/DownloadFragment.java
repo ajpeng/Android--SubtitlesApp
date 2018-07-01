f659d0033afb8f9c43f884fba12b73808dc30616
Alex Peng <jjpeng@ryerson.ca>
2018-07-01 14:49:23 -0400

    added OpenSubtitle hash

--- app/src/main/java/com/example/alex/subtitles/DownloadFragment.java
+++ app/src/main/java/com/example/alex/subtitles/DownloadFragment.java
@@ -3,6 +3,7 @@ package com.example.alex.subtitles;
 import android.app.Fragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,9 +17,13 @@ import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class DownloadFragment extends android.support.v4.app.Fragment {
@@ -28,8 +33,8 @@ public class DownloadFragment extends android.support.v4.app.Fragment {
     ListView resultList;
 
 //
-//    public DownloadFragment(){
-//    }
+    public DownloadFragment(){
+    }
 //
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_one , container, false);
@@ -41,6 +46,15 @@ public class DownloadFragment extends android.support.v4.app.Fragment {
         for(String animal : animals){
             list.add(animal);
         }
+        DownloadTask dt = new DownloadTask();
+        try {
+            list.add(dt.execute("the+expanse").get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,list);
         resultList.setAdapter(adapter);
 
@@ -51,36 +65,41 @@ public class DownloadFragment extends android.support.v4.app.Fragment {
             }
         });
 
-//        try {
-//            String dt = new DownloadTask().get();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } catch (ExecutionException e) {
-//            e.printStackTrace();
-//        }
         return view;
     }
-//
-//    public class DownloadTask extends AsyncTask<String, Void, String>{
-//
-//        @Override
-//        protected String doInBackground(String... strings) {
-//            try{
-//                String[] animals= {"cat" ,"dog", "bee", "antelope"};
-//                final ArrayList list = new ArrayList<String>();
-//                for(String animal : animals){
-//                    list.add(animal);
-//                }
-//
-//                //ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,list);
-//                //resultList.setAdapter(adapter);
-//                //Document doc = Jsoup.connect("http://google.com").get();
-//                //return doc.outerHtml();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            return null;
-//        }
-//    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try{
+                //Document doc = Jsoup.connect("http://google.com").get();
+                Log.d(TAG , "doInBackground");
+                Document doc = Jsoup.connect("https://www.opensubtitles.org/en/search2/sublanguageid-all/moviename-" + strings[0]).userAgent("TemporaryUserAgent").get();
+                getResultList(doc);
+                return doc.outerHtml().toString();
+                //return strings[0];
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return "failed to complete task";
+        }
+
+        public List<String> getResultList(Document doc){
+            List resultList = new LinkedList<>();
+            //table id="search_results" is the table which contains the content
+            for( Element table: doc.select("#search_results")){
+                for (Element row: table.select("tr")){
+                    Elements tds = row.select("td");
+                    if(tds.size() >= 4){
+                        resultList.add(tds.get(0).text() + " : " + tds.get(1).text());
+                        Log.d(TAG,tds.get(0).text() + " : " + tds.get(1).text());
+                    }
+                }
+            }
+
+            return resultList;
+        }
+    }
 
 }
